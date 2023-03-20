a2e6f5f5a819c71897fd2d1ab510d80325b7265b
Andrés Jiménez Figueroa <ajimenezf95@gmail.com>
2023-03-20 16:21:34 -0600

    Agregar archivos de proyecto.

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [DisplayName("Category name")]
+        public String CategoryName { get; set; }
+        [Required]
+        [DisplayName("Display order")]
+        [Range(0,int.MaxValue, ErrorMessage = "Display order must be greater than 0")]
+        public int DisplayOrder { get; set; }
+    }
+}
